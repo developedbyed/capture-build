b91c8747f6e2cee1f7bacbd62d222f374ce4f040
developedbyed <ed.magician@gmail.com>
2020-10-12 18:11:41 +0300

    services style

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import {About,Description,Image} from '../styles';
+import styled from 'styled-components'
 //Images
 import homeImg1 from '../img/home1.png';
-//Styled
-import styled from 'styled-components';
+
 
 
 const AboutSection = () => {
@@ -31,31 +32,7 @@ const AboutSection = () => {
     );
 }
 
-const About = styled.div`
 
-    min-height: 90vh;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 5rem 10rem;
-    color: white;
-`
-const Description = styled.div`
-    flex: 1;
-    padding-right: 5rem;
-    h2{
-        font-weight: lighter;
-    }
-`
-const Image = styled.div`
-    flex: 1;
-    overflow: hidden;
-    img{
-        width: 100%;
-        height: 80vh;
-        object-fit: cover;
-    }
-`
 const Hide = styled.div`
     overflow: hidden;
 `
--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {About,Description,Image} from '../styles';
+import styled from 'styled-components';
 //Import Icons
 import clock from '../img/clock.svg';
 import diaphragm from '../img/diaphragm.svg';
@@ -8,47 +10,80 @@ import homeImg2 from '../img/home2.png'
 
 const ServicesSection = () => {
     return(
-        <div>
-            <div className="services">
-                <div className="description">
+            <Services>
+                <Description>
                     <h2>High <span>quality</span> services.</h2>
-                    <div className="cards">
-                        <div className="card">
+                    <Cards>
+                        <Card>
                             <div className="icon">
                                 <img src={clock} alt=""/>
                                 <h3>Efficient</h3>
                             </div>
                             <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <div className="card">
+                        </Card>
+                        <Card>
                             <div className="icon">
                                 <img src={teamwork} alt=""/>
                                 <h3>Teamwork</h3>
                             </div>
                             <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <div className="card">
+                        </Card>
+                        <Card>
                             <div className="icon">
                                 <img src={diaphragm} alt=""/>
                                 <h3>Pro Grade Gear</h3>
                             </div>
                             <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <div className="card">
+                        </Card>
+                        <Card>
                             <div className="icon">
                                 <img src={money} alt=""/>
                                 <h3>Affordable</h3>
                             </div>
                             <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
-                    </div>
-                    <div className="image">
-                        <img src={homeImg2} alt="camera"/>
-                    </div>
-                </div>
-            </div>
-        </div>
+                        </Card>
+                    </Cards>
+                </Description>
+                <Image>
+                    <img src={homeImg2} alt="camera"/>
+                </Image>
+            </Services>
     )
 }
+
+const Services = styled(About)`
+     min-height: 90vh;
+     color: white;
+     display: flex;
+     justify-content: space-between;
+     align-items:center;
+     padding: 5rem 10rem;
+     h2{
+         padding-bottom: 5rem;
+     }
+     p {
+    width: 70%;
+    padding: 2rem 0rem 4rem 0rem;
+  }
+`
+
+const Cards = styled.div`
+    display:flex;
+    flex-wrap: wrap;
+`
+const Card = styled.div`
+    flex-basis: 20rem;
+    .icon{
+        display:flex;
+        align-items:center;
+        h3{
+            margin-left: 1rem;
+            background:white;
+            color:black;
+            padding: 1rem;
+        }
+    }
+`
+
 
 export default ServicesSection;
