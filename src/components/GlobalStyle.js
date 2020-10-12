492cb5b5bd98b69443284ade0aae7f4cd9070a72
developedbyed <ed.magician@gmail.com>
2020-10-12 17:51:27 +0300

    Styled Components Intro

---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,52 @@
+import {createGlobalStyle} from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    *{
+        margin:0;
+        padding:0;
+        box-sizing:border-box;
+    }
+    body{
+        background: #1b1b1b;
+        font-family: 'Inter', sans-serif;
+    }
+    button{
+        font-family: 'Inter', sans-serif;
+        font-weight: bold;
+        font-size: 1.1rem;
+        cursor: pointer;
+        padding: 1rem 2rem;
+        border: 3px solid #23d997;
+        background: transparent;
+        color: white;
+        transition: all 0.5s ease;
+        &:hover {
+        background: #23d997;
+        color: white;
+        }
+    }
+    h2{
+        font-weight: lighter;
+        font-size: 4rem;
+    }
+    h3{
+        color:white;
+    }
+    p{
+        padding: 3rem 0rem;
+        color: #cccccc;
+        font-size: 1.4rem;
+        line-height: 150%;
+    }
+    h4{
+        font-size: 2.5rem;
+    }
+    span{
+        font-weight: bold;
+        color: #23d997;
+    }
+    a{
+        font-size: 1.1.rem;
+    }
+`
+export default GlobalStyle;
