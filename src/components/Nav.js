327496ddbf79f01d5bdc11b137167b058287839b
lukm4nz3r0 <laruku.asep@gmail.com>
2020-10-17 22:44:56 +0700

    Update Nav.js
    remove number in redirect button label

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -15,7 +15,7 @@ const Nav = () => {
       </h1>
       <ul>
         <li>
-          <Link to="/">1. About us</Link>
+          <Link to="/">About us</Link>
           <Line
             transition={{ duration: 0.75 }}
             initial={{ width: '0%' }}
@@ -23,7 +23,7 @@ const Nav = () => {
           />
         </li>
         <li>
-          <Link to="/work">2. Our Work</Link>
+          <Link to="/work">Our Work</Link>
           <Line
             transition={{ duration: 0.75 }}
             initial={{ width: '0%' }}
@@ -31,7 +31,7 @@ const Nav = () => {
           />
         </li>
         <li>
-          <Link to="/contact">1. Contact Us</Link>
+          <Link to="/contact">Contact Us</Link>
           <Line
             transition={{ duration: 0.75 }}
             initial={{ width: '0%' }}
